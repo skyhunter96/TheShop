751fa0f4d18f5a351ffdbac550764e4e4c93197f
giomlly <mladen.karic@intelisale>
2022-12-21 18:36:44 +0100

    TheShop - Vendor WebApi Fixes

--- Vendor.WebApi/Program.cs
+++ Vendor.WebApi/Program.cs
@@ -1,5 +1,4 @@
 using MediatR;
-using System.Reflection;
 using Vendor.Application.Interfaces;
 using Vendor.Infrastructure.Services;
 using ILogger = Vendor.Application.Interfaces.ILogger;
@@ -9,34 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped(typeof(IDatabaseDriver), typeof(DatabaseDriver));
-builder.Services.AddScoped(typeof(ILogger), typeof(Logger));
-builder.Services.AddScoped(typeof(ISupplierService), typeof(SupplierService));
+builder.Services.AddTransient(typeof(IDatabaseDriver), typeof(DatabaseDriver));
+builder.Services.AddTransient(typeof(ILogger), typeof(Logger));
+builder.Services.AddTransient(typeof(ISupplierService), typeof(SupplierService));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger(c => c.RouteTemplate = string.Concat("vendor", "/swagger/{documentName}/swagger.json"));
-    //app.UseSwaggerUI(c =>
-    //{
-    //    c.SwaggerEndpoint($"/vendor/swagger/v1/swagger.json", "Vendor vendor API V1");
-    //    c.RoutePrefix = "vendor";
-    //});
-
     app.UseSwagger();
-    //app.UseSwaggerUI(options =>
-    //{
-    //    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    //    options.RoutePrefix = string.Empty;
-    //});
     app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "PlaceInfo Services"));
 }
 app.UseHttpsRedirection();
