b5f30267e5fdfefca847f66fe356523fc4afc896
giomlly <mladen.karic@intelisale>
2022-12-21 13:55:24 +0100

    TheShop - Vendor Domain Layer Addition

---
+++ Vendor.Domain/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿namespace Vendor.Domain.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+        public bool IsSold { get; set; }
+
+        public DateTime SoldDate { get; set; }
+        public int BuyerUserId { get; set; }
+    }
+}
